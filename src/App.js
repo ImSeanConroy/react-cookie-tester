aef0378720f303c73b955fde9348c6144beb698b
Sean Conroy <seanconroy2001@gmail.com>
2023-02-19 14:39:53 +0000

    Create basic consent pop up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import ReactGA from "react-ga4";
+import CookieConsent, {
+  getCookieConsentValue,
+  Cookies,
+} from "react-cookie-consent";
 
 function App() {
+  const handleAcceptCookie = () => {
+    ReactGA.initialize(process.env.REACT_APP_GA_ID);
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname,
+    });
+  };
+
+  const handleDeclineCookie = () => {
+    //remove google analytics cookies
+    Cookies.remove("_ga");
+    Cookies.remove("_gat");
+    Cookies.remove("_gid");
+  };
+
+  useEffect(() => {
+    const isConsent = getCookieConsentValue();
+    if (isConsent === "true") {
+      handleAcceptCookie();
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +47,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <CookieConsent
+        debug={true}
+        enableDeclineButton
+        onAccept={handleAcceptCookie}
+        onDecline={handleDeclineCookie}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
     </div>
   );
 }
